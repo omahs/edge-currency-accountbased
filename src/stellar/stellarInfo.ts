390288097735d356a07e17486a97c0bc257a2625
William Swanson <swansontec@gmail.com>
2023-08-24 13:54:12 -0700

    fixup! Add modern memo handling to all plugins

--- src/stellar/stellarInfo.ts
+++ src/stellar/stellarInfo.ts
@@ -29,9 +29,9 @@ export const currencyInfo: EdgeCurrencyInfo = {
 
   // https://developers.stellar.org/docs/encyclopedia/memos
   memoOptions: [
-    { type: 'hex', maxBytes: 32, minBytes: 32 },
+    { type: 'text', maxLength: 28 },
     { type: 'number', maxValue: '18446744073709551615' },
-    { type: 'text', maxLength: 28 }
+    { type: 'hex', maxBytes: 32, minBytes: 32 }
     // We also support a transaction ID for returned funds
   ],
   multipleMemos: true,
